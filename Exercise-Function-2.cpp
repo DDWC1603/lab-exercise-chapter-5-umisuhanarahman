b3b070cb017b823ae0b6e28f7dc785f3839703c6
umisuhanarahman <33883955+umisuhanarahman@users.noreply.github.com>
2018-01-16 12:01:37 +0800

    task completed

--- Exercise-Function-2.cpp
+++ Exercise-Function-2.cpp
@@ -1,9 +1,22 @@
-//this is an unfinish program
-//finish up this program
+//umi suhana binti abdul rahman
+//A17DW2041
+
+#include<iostream>
+using namespace std;
 
 int sum(int x,int y)
 {
 	int result;
 	result = x+y;
+	cout<<"x+y = "<<result<<endl;
+}
 
+int main()
+{
+	int x = 5;
+	int y = 4;
+	
+	sum(x,y);
+	
+	return 0;
 }
